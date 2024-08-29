b31cc45d254c9346c43678cbef2688efcd6d5907
rodrigo-sousa-emf <rodrigo.sousa@studentfr.ch>
2024-08-29 10:31:55 +0200

    exercice06

---
+++ exercices/exercice06/exerciceswitch01.java
@@ -0,0 +1,27 @@
+package exercices.exercice06;
+
+public class exercice06 {
+    public static void main(String[] args) {
+        int  notedemodule =4;
+switch (notedemodule) {
+ case 1:
+System.out.println("travail non rendu");
+break;
+case 2:
+    System.out.println("trés insuffisant");
+    break;
+    case 3:
+        System.out.println("Insuffisant");
+        break;
+       case 4:
+            System.out.println("suffisant");
+            break;
+            case 5 :
+                System.out.println("bien");
+                break;
+                case 6 :
+                    System.out.println("trés bien");
+                    break;
+                
+    }
+}
---
+++ exercices/exercice06/exerciceswitch2.java
@@ -0,0 +1,23 @@
+package exercices.exercice06;
+
+public class exerciceswitch2 {
+    public static void main(String[] args) {
+        int age = 7;
+        switch(age){
+        case 7:
+        System.out.println("poussin");
+        break;
+        case 8:
+        case 9:
+            System.out.println("pupille");
+            break;
+        case 10:
+        case 11:
+        System.out.println("Minime");
+        break;
+        default:
+        System.out.println("inconnue");
+        break;
+} 
+}
+ }
