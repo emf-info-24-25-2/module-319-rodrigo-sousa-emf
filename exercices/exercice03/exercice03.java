97e0cad3bd4dfd1095ca7295d7254c0d8ef07432
rodrigo-sousa-emf <rodrigo.sousa@studentfr.ch>
2024-08-29 09:48:05 +0200

    exercice03

---
+++ exercices/exercice03/exercice03.java
@@ -0,0 +1,11 @@
+package exercices.exercice03;
+
+public class exercice03 {
+    public static void main(String[] args) {
+        int age = 17;
+        String nom = "Rodrigo";
+        System.out.println("Je m'appelle " + nom + " et j'ai " + age + " ans");
+        System.out.println( "Dans 10 ans, j'aurais " + (age+10) + " ans");
+    
+    }
+}
