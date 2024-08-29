581589bfa7bbee3178534c29de475b7b63105985
rodrigo-sousa-emf <rodrigo.sousa@studentfr.ch>
2024-08-29 09:37:26 +0200

    exercice02

---
+++ exercices/exercice02/exercice02.java
@@ -0,0 +1,8 @@
+package exercices.exercice02;
+
+public class exercice02 {
+    public static void main(String[] args) {
+        System.out.println("Bonjour tout le monde le module 319 est simple pour le moment");
+   
+    }
+}
