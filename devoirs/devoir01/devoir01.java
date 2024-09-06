4fb998fe289b601fbf77658ce7b21b68dee95bcf
rodrigo-sousa-emf <rodrigo.sousa@studentfr.ch>
2024-09-06 15:34:01 +0200

    devoir

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -3,7 +3,7 @@ package devoirs.devoir01;
 public class devoir01 {
     public static void main(String[] args) {
         int monAge = 30;
-        boolean estMajeur;
+        boolean estMajeur; 
  
         if (monAge >= 20) {
             estMajeur = true; 
