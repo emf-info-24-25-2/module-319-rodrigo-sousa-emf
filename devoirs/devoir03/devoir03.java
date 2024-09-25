5d9478656768ea1572e81ba5e23fc873130cea5e
Rodrigo <rodrigo.sousa@studentfr.ch>
2024-09-25 18:00:35 +0200

    devoir03

---
+++ devoirs/devoir03/devoir03.java
@@ -0,0 +1,52 @@
+public class devoir03 {
+    public static void main(String[] args) {
+   
+   int jour = 1;
+   int mois = 2;
+   int Années = 2024;
+   if (mois == 1){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour +" de l'An."+ Années);
+   }
+   else if (mois == 2){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31 +" de l'An."+ Années);
+   }
+   else if (mois == 3){
+       int fev = jour + 31;
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ fev +" de l'An."+ Années);
+   }
+   else if (mois == 3){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31+28 +" de l'An."+ Années);
+   }
+   else if (mois == 4){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31 +28 + 31 +" de l'An."+ Années);
+   }
+   else if (mois == 5){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31 +28 + 31 +30 +" de l'An."+ Années);
+   }
+   else if (mois == 6){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31 +28 + 31 +30+31+" de l'An."+ Années);
+   }
+   else if (mois == 7){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31 +28 + 31 +30+31+30+" de l'An."+ Années);
+   }
+   else if (mois == 8){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31 +28 + 31 +30+31+30+31+" de l'An."+ Années);
+   }
+   else if (mois == 9){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31 +28 + 31 +30+31+30+31+30+" de l'An."+ Années);
+   }
+   else if (mois == 10){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31 +28 + 31 +30+31+30+31+30+31+" de l'An."+ Années);
+   }
+   else if (mois == 11){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31 +28 + 31 +30+31+30+31+30+31+30+" de l'An."+ Années);
+   }
+   else if (mois == 12){
+   System.out.println("Le"+jour +"/"+mois+"/"+"/"+Années+"est le jour "+ jour + 31 +28 + 31 +30+31+30+31+30+31+30+31+" de l'An.");
+   }
+   
+   
+   }
+   
+    }
+    
